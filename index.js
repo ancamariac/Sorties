2d5ab326dde49f1d51581ea56a2ff75b3b9de7ee
ancamariac <ciuciuancamaria@gmail.com>
2022-01-21 13:35:41 +0200

    Changed some queries

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/home', function(request, response) {
 					connection.query('SELECT Nume, Prenume, Denumire, CNP, Angajat_ID FROM angajati JOIN departamente on angajati.Departament_ID = departamente.Departament_ID AND angajati.Angajat_ID!=? AND angajati.Departament_ID=?', 
 					[request.session.angajatID, request.session.departament_id], function(error, results_angajati, fields) {
 
-						connection.query("SELECT sarcini.Detalii, sarcini.Sarcina_ID, departamente.Denumire FROM sarcini JOIN servicii on sarcini.Serviciu_ID = servicii.Serviciu_ID JOIN departamente on servicii.DepartamentID = departamente.Departament_ID WHERE sarcini.Status = 'Nefinalizat' AND servicii.DepartamentID=?", 
+						connection.query("SELECT sarcini.Detalii, day(sarcini.Data) as Zi, month(sarcini.Data) as Luna, year(sarcini.Data) as An, sarcini.Sarcina_ID, departamente.Denumire FROM sarcini JOIN servicii on sarcini.Serviciu_ID = servicii.Serviciu_ID JOIN departamente on servicii.DepartamentID = departamente.Departament_ID WHERE sarcini.Status = 'Nefinalizat' AND servicii.DepartamentID=?", 
 						[request.session.departament_id], function(error, results_sarcini, fields) {
 
 							connection.query("SELECT sarcini.Detalii, day(sarcini.Data) as Zi, month(sarcini.Data) as Luna, year(sarcini.Data) as An, Nume, Prenume FROM sarcini JOIN `servicii` on `servicii`.`Serviciu_ID` = sarcini.Serviciu_ID JOIN `angajati-sarcini` on sarcini.Sarcina_ID = `angajati-sarcini`.`Sarcina_ID` JOIN `angajati` on angajati.Angajat_ID = `angajati-sarcini`.`Angajat_ID` WHERE sarcini.Status = 'In procesare' AND servicii.DepartamentID=?", 
